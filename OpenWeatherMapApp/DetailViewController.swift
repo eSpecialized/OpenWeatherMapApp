7c84445c004cb42338c2b82ea8b10fb4c7c89365
Bill Thompson <wthomps@willandnora.com>
2018-10-22 21:25:04 -0700

    Initial Project Commit

---
+++ OpenWeatherMapApp/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  OpenWeatherMapApp
+//
+//  Created by William Thompson on 10/22/18.
+//  Copyright © 2018 William Thompson. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '11.0'
+
+target 'OpenWeatherMapApp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for OpenWeatherMapApp
+   pod 'SwiftyGif', '~> 4.2'
+   pod 'RxSwift', '~> 4.3.1'
+   pod 'RxCocoa', '~> 4.3.1'
+   pod 'RealmSwift', '~> 3.11.1'
+
+  target 'OpenWeatherMapAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'OpenWeatherMapAppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
